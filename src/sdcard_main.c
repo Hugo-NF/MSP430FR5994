7214c7666bdfb5e157ed0e422f17bb0f7f107667
Hugo-NF <hugonfonseca@hotmail.com>
2021-03-11 10:30:27 -0300

    🚧 Add SD card utilities sample

---
+++ src/sdcard_main.c
@@ -0,0 +1,202 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2016, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+/*******************************************************************************
+ *
+ * main.c
+ *
+ * Basic write to SDCard example.
+ * Uses an array of data to emulate ADC values.
+ * Loop through the array and write data to SDCard.
+ *
+ * Creates directory name "Samples"
+ * Creates filename "testfile.txt"
+ *
+ * Note: User must develop own routine to check for file overwrite protection
+ * else each time code runs, previous data is erased.
+ ******************************************************************************/
+
+//#include "main.h"
+#include <SDCard_API.h>
+#include "driverlib.h"
+
+void Init_GPIO(void);
+void Init_Clock(void);
+
+
+uint8_t testData = 0;
+char filename[] = {"testfile.txt"};
+char directory[] = {"samples"};
+char buffer[48];
+uint16_t exampleData[] = {1,200,500,20,75,300,1000,880,234,700};
+char asciiData[64];
+
+// Initialize SDCard and File status with default values
+SDCardLib_Status SDCard_status = SDCARDLIB_STATUS_NOT_PRESENT;
+FRESULT File_status = FR_OK;
+bool Success = false;
+
+//-----------------------------------------------------------------------------
+int _system_pre_init(void)
+{
+    // Stop Watchdog timer
+    WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__);     // Stop WDT
+
+    GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0);
+
+    /*==================================*/
+    /* Choose if segment initialization */
+    /* should be done or not. */
+    /* Return: 0 to omit initialization */
+    /* 1 to run initialization */
+    /*==================================*/
+    return 1;
+}
+
+/*
+ * main.c
+ */
+
+int main(void) {
+
+    uint8_t sizeOfData = sizeof(exampleData) >> 1;  // Total bytes/2 = #uint16_t entries in the array
+    uint8_t i;
+
+    Init_GPIO();
+
+    Init_Clock();
+
+    // Set Success true to start
+    Success = true;
+
+    SDCard_status = createFile_SDCard(directory, filename);
+    // Check for SDCard and file errors
+    if((SDCard_status == SDCARDLIB_STATUS_NOT_PRESENT) || (fr != FR_OK))
+    {
+         Success = false;
+         _no_operation();
+    }
+
+    if(Success == true)
+    {
+        // Emulate ADC Measure and write to SDCard
+        // Use pre-filled exampleData[] buffer as ADC data
+        for(i = 0; i< sizeOfData; i++)
+        {
+            // Convert ADC sample to ascii
+            itoa(exampleData[i], asciiData, 10);
+
+            // Write data to SDCard
+            SDCard_status = write_SDCard(asciiData);
+
+            // Check for SDCard and file errors
+            if(SDCard_status == SDCARDLIB_STATUS_NOT_PRESENT )
+                break;  // something went wrong
+            else if(fr != FR_OK)
+                break;    // something went wrong
+            else
+                _no_operation();  // Place to do other things.
+
+        }
+        GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN1);  // Turn on GREEN LED
+    }
+    else
+        GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);  // Turn on RED LED
+
+    // Done - sit here in loop
+    while(1);
+
+
+}
+
+/*
+ * GPIO Initialization
+ */
+void Init_GPIO()
+{
+    // Set all GPIO pins to output low for low power
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_PJ, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7|GPIO_PIN8|GPIO_PIN9|GPIO_PIN10|GPIO_PIN11|GPIO_PIN12|GPIO_PIN13|GPIO_PIN14|GPIO_PIN15);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN0);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_PJ, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7|GPIO_PIN8|GPIO_PIN9|GPIO_PIN10|GPIO_PIN11|GPIO_PIN12|GPIO_PIN13|GPIO_PIN14|GPIO_PIN15);
+
+	// Configure P2.0 - UCA0TXD and P2.1 - UCA0RXD
+	GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+	GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P2, GPIO_PIN1, GPIO_SECONDARY_MODULE_FUNCTION);
+
+    // Set PJ.4 and PJ.5 as Primary Module Function Input, LFXT.
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+           GPIO_PORT_PJ,
+           GPIO_PIN4 + GPIO_PIN5,
+           GPIO_PRIMARY_MODULE_FUNCTION
+           );
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    PMM_unlockLPM5();
+}
+
+/*
+ * Clock System Initialization
+ */
+void Init_Clock()
+{
+    // Set DCO frequency to 8 MHz
+    CS_setDCOFreq(CS_DCORSEL_0, CS_DCOFSEL_6);
+    //Set external clock frequency to 32.768 KHz
+    CS_setExternalClockSource(32768, 0);
+    //Set ACLK=LFXT
+    CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    // Set SMCLK = DCO with frequency divider of 1
+    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    // Set MCLK = DCO with frequency divider of 1
+    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    //Start XT1 with no time out
+    CS_turnOnLFXT(CS_LFXT_DRIVE_3);
+}
